96053bd3a5dc0162c110276a2e51dad2960dff3a
Semhal22 <123024495+Semhal22@users.noreply.github.com>
2023-07-02 20:28:41 +0300

    Reverse linked list iteratively

--- designLinkedList.py
+++ designLinkedList.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self):
+    def __init__(self, val):
         self.val = val
         self.next = None
 
@@ -9,7 +9,7 @@ class MyLinkedList(object):
         self.head = None
         self.size = 0        
 
-    def get(self, index: int) -> int:
+    def get(self, index):
         """
         :type index: int
         :rtype: int
@@ -21,7 +21,7 @@ class MyLinkedList(object):
             temp = temp.next
         return temp.val
 
-    def addAtHead(self, val: int) -> int:
+    def addAtHead(self, val):
         """
         :type val: int
         :rtype: None
@@ -49,7 +49,7 @@ class MyLinkedList(object):
         
         
 
-    def addAtIndex(self, index: int, val:int) -> None:
+    def addAtIndex(self, index, val):
         """
         :type index: int
         :type val: int
